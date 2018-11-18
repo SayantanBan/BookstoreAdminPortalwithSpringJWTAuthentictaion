e45e798621a01ea65d99907581e7ddafb68b6a20
Sayantan Banerjee <sayantanbanerjee@outlook.com>
2018-11-18 19:28:17 +0530

    Update RequestFilter.java

--- src/main/java/com/bookstore/config/RequestFilter.java
+++ src/main/java/com/bookstore/config/RequestFilter.java
@@ -21,7 +21,7 @@ public class RequestFilter implements Filter{
 		
 		response.setHeader("Access-Control-Allow-Origin", "https://bookstoreadmin-e49b8.firebaseapp.com");
 		response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
-		response.setHeader("Access-Control-Allow-Headers", "x-requested-with, x-auth-token");
+		response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers", "x-requested-with, x-auth-token");
 		response.setHeader("Access-Control-Max-Age", "3600");
 		response.setHeader("Access-Control-Allow-Credentials", "true");
 		
